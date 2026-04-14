ff7aa3f6f70138e69bd9010dba043f6c2cf0abe2
DocSvartz <svartzdoctor@gmail.com>
2026-04-14 12:12:00 +0500

    add TypeConverter

---
+++ src/Mapster.Compile.Configuration/TypeConverter.cs
@@ -0,0 +1,13 @@
+﻿using Mapster.Compile.Config;
+using Mapster.Compile.Configuration.Inspectors;
+
+namespace Mapster.Compile.Configuration
+{
+    internal class TypeConverter
+    {
+        public TypeInspector SourceType {  get; }
+        public TypeInspector TargetType { get; }
+        public TypeAdapterSettingsCompile Settings {  get; }
+        public TypeAdapterConfig ContextConfigs { get; }
+    }
+}
