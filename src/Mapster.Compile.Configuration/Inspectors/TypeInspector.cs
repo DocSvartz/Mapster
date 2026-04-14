71471e049bfedab0a76588ab32fd27c611bc6d69
DocSvartz <svartzdoctor@gmail.com>
2026-04-14 11:52:01 +0500

    add TypeInspector

---
+++ src/Mapster.Compile.Configuration/Inspectors/TypeInspector.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mapster.Compile.Configuration.Inspectors
+{
+    internal class TypeInspector : IEquatable<TypeInspector>
+    {
+        public TypeInspector (ITypeSymbol typeSymbol)
+        {
+            IsPartial = getPartial (typeSymbol);
+            Type = (INamedTypeSymbol)typeSymbol;
+        }
+
+        public INamedTypeSymbol Type { get; }
+        public bool IsPartial { get; }
+        public IEnumerable<ISymbol> Members { get => Type.GetMembers().Where(x => (x is IPropertySymbol || x is IFieldSymbol) && !x.MetadataName.EndsWith(">k__BackingField")); }
+        public IEnumerable<IMethodSymbol> Constructors { get => Type.Constructors; }
+        public IEnumerable<IFieldSymbol> BackFields { get => Type.GetMembers().OfType<IFieldSymbol>().Where(x=> x.MetadataName.EndsWith(">k__BackingField")); }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as TypeInspector);
+        }
+
+        public bool Equals(TypeInspector other)
+        {
+            return other is not null &&
+                   EqualityComparer<INamedTypeSymbol>.Default.Equals(Type, other.Type);
+        }
+
+        public override int GetHashCode()
+        {
+            return 2049151605 + EqualityComparer<INamedTypeSymbol>.Default.GetHashCode(Type);
+        }
+
+        private bool getPartial (ITypeSymbol type)
+        {
+            foreach (var reference in type.DeclaringSyntaxReferences)
+            {
+                var node = reference.GetSyntax();
+                if (node is ClassDeclarationSyntax classDecl &&
+                    classDecl.Modifiers.Any(m => m.IsKind(SyntaxKind.PartialKeyword)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator ==(TypeInspector left, TypeInspector right)
+        {
+            return EqualityComparer<TypeInspector>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(TypeInspector left, TypeInspector right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ src/Mapster.Compile.Configuration/TypeAdapterConfig.cs
@@ -0,0 +1,45 @@
+﻿using Mapster.Configuration;
+using System;
+
+namespace Mapster.Compile.Configuration
+{
+    public class TypeAdapterConfig : ITypeAdapterConfigBase
+    {
+        public bool AllowImplicitDestinationInheritance { get; set; }
+        public bool AllowImplicitSourceInheritance { get; set; }
+        public TypeAdapterSetter Default => throw new NotImplementedException();
+        public bool RequireDestinationMemberSource { get; set; }
+        public bool RequireExplicitMapping { get; set; }
+        public bool RequireExplicitMappingPrimitive { get; set; }
+
+        public TypeAdapterSetter ForDestinationType(Type destinationType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeAdapterSetter<TDestination> ForDestinationType<TDestination>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeAdapterSetter ForType(Type sourceType, Type destinationType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeAdapterSetter<TSource, TDestination> ForType<TSource, TDestination>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeAdapterSetter NewConfig(Type sourceType, Type destinationType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeAdapterSetter<TSource, TDestination> NewConfig<TSource, TDestination>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
