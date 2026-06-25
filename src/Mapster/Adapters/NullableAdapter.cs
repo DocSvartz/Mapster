c670690c40a5f1371e9adcf0c21e767e2b196ac7
DocSvartz <svartzdoctor@gmail.com>
2026-06-25 12:03:03 +0500

    fix: quick fix Custom converter supporting from nullable ValueType

--- src/Mapster/Adapters/NullableAdapter.cs
+++ src/Mapster/Adapters/NullableAdapter.cs
@@ -1,4 +1,5 @@
-﻿using Mapster.Utils;
+﻿using Mapster.Models;
+using Mapster.Utils;
 using System.Linq.Expressions;
 
 namespace Mapster.Adapters
@@ -19,6 +20,22 @@ namespace Mapster.Adapters
 
         protected override Expression? CreateInlineExpression(Expression source, CompileArgument arg, bool IsRequiredOnly = false)
         {
+            if (arg.ExplicitMapping)
+            {
+                LambdaExpression? Convert = null;
+
+                TypeAdapterRule? getsettings;
+                arg.Context.Config.RuleMap.TryGetValue(new TypeTuple(arg.SourceType, arg.DestinationType), out getsettings);
+
+                if (getsettings != null)
+                    if (arg.MapType == MapType.MapToTarget)
+                        Convert = getsettings.Settings.ConverterToTargetFactory(arg);
+                    else
+                        Convert = getsettings.Settings.ConverterFactory(arg);
+                if (Convert != null)
+                    return Convert.Apply(arg.MapType, source);
+            }
+
             var _source = source.Type.IsNullable() 
                 ? Expression.Convert(source, source.Type.GetGenericArguments()[0]) 
                 : source;
