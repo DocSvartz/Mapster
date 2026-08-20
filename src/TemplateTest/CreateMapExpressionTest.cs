2d206257f9cbda223898a0a4d3338b791456df20
DocSvartz <svartzdoctor@gmail.com>
2026-08-20 17:10:44 +0500

    add internal omly

--- src/TemplateTest/CreateMapExpressionTest.cs
+++ src/TemplateTest/CreateMapExpressionTest.cs
@@ -5,7 +5,9 @@ using Mapster.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace TemplateTest
 {
@@ -85,13 +87,14 @@ namespace TemplateTest
                 Implements = new[] { typeof(IMyTypeMapper) },
                 Namespace = "Benchmark",
                 TypeName = "CustomerMapper",
-                IsInternal = true,
+                IsInternal = false,
                 GeneratedAttributes = new(new[] {new MapsterToolGeneratedMapperAttribute()})
             };
 
             var translator = new ExpressionTranslator(definitions);
 
-            foreach (var method in typeof(IMyTypeMapper).GetMethods())
+            foreach (var method in typeof(IMyTypeMapper).GetMethods(BindingFlags.Public|BindingFlags.NonPublic|BindingFlags.Instance)
+                .Where(x=>x.IsPublicOrInternal()))
             {
                 if (method.IsGenericMethod)
                     continue;
@@ -113,7 +116,9 @@ namespace TemplateTest
                 );
             }
 
-            foreach (var prop in typeof(IMyTypeMapper).GetProperties())
+            foreach (var prop in typeof(IMyTypeMapper).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(x=>x.IsGetterPublicOrInternal())
+                )
             {
                 if (!prop.PropertyType.IsGenericType)
                     continue;
@@ -138,21 +143,95 @@ namespace TemplateTest
 
             var txt = translator.ToString();
 
+            var src = new Address() { City = "City 17"};
+
+            IMyTypeMapper mapper = new CustomerMapper();
+
+            var result = mapper.Map(src);
+
+
             Assert.IsTrue(txt.Contains("Expression<Func<AddressDTO, Address>> TemplateTest.IMyTypeMapper.Projection"));
             Assert.IsTrue(txt.Contains("AddressDTO TemplateTest.IMyTypeMapper.Map"));
             Assert.IsTrue(txt.Contains("[MapsterToolGeneratedMapper]"));
-
         }
 
     }
 
-    internal interface IMyTypeMapper
+    public partial class CustomerMapper : IMyTypeMapper
     {
-        AddressDTO Map(Address p1);
-        Expression<Func<AddressDTO, Address>> Projection { get; }
+        
+        AddressDTO TemplateTest.IMyTypeMapper.Map(Address p1)
+        {
+            return p1 == null ? null : new AddressDTO()
+            {
+                Id = p1.Id,
+                City = p1.City,
+                Country = p1.Country
+            };
+        }
+        
+        Expression<Func<AddressDTO, Address>> TemplateTest.IMyTypeMapper.Projection => p2 => new Address()
+        {
+            Id = p2.Id,
+            City = p2.City,
+            Country = p2.Country
+        };
     }
 
-    public class Address
+
+
+
+    public static class MethodInfoExtensions
+    {
+        public static bool IsPublicOrInternal(this MethodInfo method)
+        {
+            if (method == null) throw new ArgumentNullException(nameof(method));
+
+            return !method.IsPrivate
+                   && !method.IsFamily
+                   && !method.IsFamilyOrAssembly
+                   && !method.IsFamilyAndAssembly
+                   && (method.IsPublic || true);
+        }
+
+       
+
+        public static bool IsGetterPublicOrInternal(this PropertyInfo property)
+        {
+            if (property == null) throw new ArgumentNullException(nameof(property));
+
+            MethodInfo? getMethod = property.GetMethod;
+
+            if (getMethod == null) return false;
+
+            return !getMethod.IsPrivate
+                   && !getMethod.IsFamily
+                   && !getMethod.IsFamilyOrAssembly
+                   && !getMethod.IsFamilyAndAssembly
+                   && (getMethod.IsPublic || true);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public interface IMyTypeMapper
+    {
+        internal AddressDTO Map(Address p1);
+        internal Expression<Func<AddressDTO, Address>> Projection { get; }
+    }
+
+    internal class Address
     {
         public int Id { get; set; }
         public string Street { get; set; }
@@ -172,10 +251,10 @@ namespace TemplateTest
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal? Credit { get; set; }
-        public Address Address { get; set; }
-        public Address HomeAddress { get; set; }
-        public Address[] Addresses { get; set; }
-        public ICollection<Address> WorkAddresses { get; set; }
+        //public Address Address { get; set; }
+        //public Address HomeAddress { get; set; }
+        //public Address[] Addresses { get; set; }
+        //public ICollection<Address> WorkAddresses { get; set; }
     }
 
     public class CustomerDTO
